ce237cab6487a7e39ab5ae1ce2f5f2db2bb95c31
gabycamps <g.camposm@uniandes.edu.co>
2024-09-27 09:01:50 -0500

    commit inicial

--- App/logic.py
+++ App/logic.py
@@ -27,9 +27,9 @@
 import os
 import csv
 
-# TODO Realice la importación del mapa linear probing
-# TODO Realice la importación de ArrayList como estructura de datos auxiliar para sus requerimientos
 
+from DataStructures.Map import map_linear_probing as lp
+from DataStructures.List import array_list as lt
 
 
 data_dir = os.path.dirname(os.path.realpath('__file__')) + '/Data/'
